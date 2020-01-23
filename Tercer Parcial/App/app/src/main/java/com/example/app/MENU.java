3fcffa0ffe4208caf7c149b5f57fc272577f049d
MateoReinoso <mateo_riva@hotmail.com>
2020-01-23 00:10:46 -0500

    android

---
+++ Tercer Parcial/App/app/src/main/java/com/example/app/MENU.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app.R;
+
+public class MENU extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
